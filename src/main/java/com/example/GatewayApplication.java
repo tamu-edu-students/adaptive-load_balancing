d1047173ec46808db2a9a49fe5f3c59ab363490e
Vinayaka Hegde <vinuvinayaka2000@gmail.com>
2025-12-12 15:39:35 -0600

    Gateway application done

--- src/main/java/com/example/GatewayApplication.java
+++ src/main/java/com/example/GatewayApplication.java
@@ -1,9 +1,3 @@
-package com.example;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 @SpringBootApplication
 @EnableScheduling
 public class GatewayApplication {
